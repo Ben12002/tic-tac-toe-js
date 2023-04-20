fa8d2489c3e681fc3a7d5eb279e0048abd85952d
Benedict Tan <bendamian2012@gmail.com>
2023-04-20 11:39:03 -0700

    wrote skeleton of program

---
+++ script.js
@@ -0,0 +1,40 @@
+function boardFactory(){
+    const arr = [];
+
+    const move = function(marker) {
+
+    }
+
+    const isGameOver = function(marker) {
+        
+    }
+}
+
+function playerFactory(marker){
+    return  { marker };
+}
+
+
+const game = (() => {
+    const playerOne = playerFactory("X");
+    const playerTwo = playerFactory("O");
+    const board = boardFactory();
+
+    const play = function(){
+        let turn = 1;
+        while (!isGameOver) {
+            let currentPlayer = (turn % 2 == 1) ? playerOne : playerTwo;
+            board.move(currentPlayer.marker);
+        }
+        win(currentPlayer);
+    }
+
+    const isGameOver = (marker) => board.isGameOver(marker);
+    const win = function(player) {
+
+    }
+})();
+
+// main
+
+game.play();
